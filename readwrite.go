4addef00d65a9234700e1a23ec199887ba61c117
liuber <liudmyla.b@arringo.com>
2023-10-21 13:27:53 +0200

    read json tests

--- readwrite.go
+++ readwrite.go
@@ -3,11 +3,14 @@ package jsonutils
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
-type JsonResponse struct {
+// JSONResponse is a type used for sending JSON around
+type JSONResponse struct {
 	Error bool     `json:"error"`
 	Message string `json:"message"`
 	Data any       `json:"data,omitempty"`
@@ -15,18 +18,61 @@ type JsonResponse struct {
 
 // Type used to instantiate the module. Any variable of this type
 // will have access to all the methods with the receiver *JsonRW
-type JsonUtils struct{}
+type JsonUtils struct{
+	MaxSize int64
+	AllowUnknownFields bool
+}
 
-func (app *JsonUtils) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
-	maxBytes := 1048576 // 1 Mb
+func (ju *JsonUtils) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	
+	maxBytes := int64(1048576) // 1 Mb
+	
+	if ju.MaxSize != 0 {
+		maxBytes = ju.MaxSize
+	}
 
-	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	r.Body = http.MaxBytesReader(w, r.Body, maxBytes)
 
 	dec := json.NewDecoder(r.Body)
+
+	if !ju.AllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
 	err := dec.Decode(data)
 
 	if err != nil {
-		return err
+		var syntaxError *json.SyntaxError
+		var unmarshaleTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+			case errors.As(err, &syntaxError):
+				return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+			case errors.Is(err, io.ErrUnexpectedEOF):
+				return errors.New("body contains badly-formated JSON")
+
+			case errors.As(err, &unmarshaleTypeError):
+				if unmarshaleTypeError.Field != "" {
+					return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshaleTypeError.Field)
+				}
+				return fmt.Errorf("body contains incorrect JSON type at character %d", unmarshaleTypeError.Offset)
+
+			case strings.HasPrefix(err.Error(), "json: unknown field"):
+				fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+				return fmt.Errorf("body contains unknown key %s", fieldName)
+
+			case err.Error() == "http: request body too large":
+				return fmt.Errorf("body mast not be larger than %d bytes", maxBytes)
+			
+			case errors.As(err, &invalidUnmarshalError):
+				return fmt.Errorf("error unmarshalling JSON: %s", err.Error())
+		
+			default:
+				return err
+
+		}
 	}
 
 	err = dec.Decode(&struct{}{})
@@ -37,7 +83,7 @@ func (app *JsonUtils) ReadJSON(w http.ResponseWriter, r *http.Request, data any)
 	return nil
 }
 
-func (app *JsonUtils) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header ) error {
+func (ju *JsonUtils) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header ) error {
 	out, err := json.Marshal(data)
 	if err != nil {
 		return err
@@ -60,16 +106,16 @@ func (app *JsonUtils) WriteJSON(w http.ResponseWriter, status int, data any, hea
 	return nil
 }
 
-func (app *JsonUtils) ErrorJSON(w http.ResponseWriter, err error, status ...int ) error {
+func (ju *JsonUtils) ErrorJSON(w http.ResponseWriter, err error, status ...int ) error {
 	statusCode := http.StatusBadRequest
 
 	if len(status) > 0 {
 		statusCode = status[0]
 	}
 
-	var payload JsonResponse
+	var payload JSONResponse
 	payload.Error = true
 	payload.Message = err.Error()
 
-	return app.WriteJSON(w, statusCode, payload)
+	return ju.WriteJSON(w, statusCode, payload)
 }
---
+++ readwrite_test.go
@@ -0,0 +1,60 @@
+package jsonutils
+
+import (
+	"testing"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+)
+
+
+var jsonTests = []struct {
+	name string
+	json string
+	maxSize int64
+	allowUnknown bool
+	errorExpected bool
+}{
+	{name:"good json", json: `{"field" : "value"}`, maxSize: 1024, allowUnknown: false, errorExpected: false},
+	{name:"badly-formated json", json: `{"field" :}`, maxSize: 1024, allowUnknown: false, errorExpected: true},
+	{name:"incorrect type", json: `{"field" : 1}`, maxSize: 1024, allowUnknown: false, errorExpected: true},
+	{name:"two json files", json: `{"field" : "val1"}{"anothe_one": "val2"}`, maxSize: 1024, allowUnknown: false, errorExpected: true},
+	{name: "empty body", json: ``, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "syntax error in json", json: `{"field": 1"`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "unknown field in json", json: `{"fooo": "1"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "allow unknown fields in json", json: `{"fooo": "1"}`, errorExpected: false, maxSize: 1024, allowUnknown: true},
+	{name: "missing field name", json: `{jack: "1"}`, errorExpected: true, maxSize: 1024, allowUnknown: true},
+	{name: "file too large", json: `{"field": "value"}`, errorExpected: true, maxSize: 5, allowUnknown: true},
+	{name: "not json", json: `Hello, world!`, errorExpected: true, maxSize: 1024, allowUnknown: true},
+}
+
+func TestJsonUtils_ReadJSON(t *testing.T) {
+	var testUtils JsonUtils
+	for _, e := range jsonTests {
+		testUtils.MaxSize = e.maxSize
+		testUtils.AllowUnknownFields = e.allowUnknown
+
+		var decodedJSON struct {
+			Field string `json:"field"`
+		}
+
+		req, err := http.NewRequest("POST", "/", bytes.NewReader([]byte(e.json)))
+		if err != nil {
+			t.Log("Error:", err)
+		}
+
+		recorder := httptest.NewRecorder()
+
+		err = testUtils.ReadJSON(recorder, req, &decodedJSON)
+
+		if e.errorExpected && err == nil {
+			t.Errorf("%s: error expected, but non received", e.name)
+		}
+
+		if !e.errorExpected && err != nil {
+			t.Errorf("%s: error not expected, but one received: %s", e.name, err.Error())
+		}
+
+		req.Body.Close()
+	}
+}
